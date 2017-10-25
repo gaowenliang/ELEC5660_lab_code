a6b4a7917ec7ff3e87b334807e699b14c2f09274
gaowenliang <gaowenliang100@gmail.com>
2017-10-25 20:38:52 +0800

    fix bugs

--- src/app/demo_application/hover.sh
+++ src/app/demo_application/hover.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/nvidia/demo_application/bashrc.sh;
+source /home/nvidia/ELEC5660_lab_code/src/app/demo_application/bashrc.sh;
 
 roslaunch mocap_optitrack mocap.launch & sleep 1;
 #rosrun trajectory_generator toOdom
