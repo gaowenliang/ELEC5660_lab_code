59028680d4839c15b032ce560f23264e74a6fed6
gaowenliang <gaowenliang100@gmail.com>
2017-10-26 11:03:58 +0800

    fix mocap bugs

--- src/app/demo_application/hover.sh
+++ src/app/demo_application/hover.sh
@@ -4,6 +4,6 @@ source /home/nvidia/ELEC5660_lab_code/src/app/demo_application/bashrc.sh;
 
 roslaunch mocap_optitrack mocap.launch & sleep 1;
 #rosrun trajectory_generator toOdom
-roslaunch pos_vel_mocap pos_vel_mocap.launch & sleep 1;
+rosrun pos_vel_mocap pos_vel_mocap & sleep 1;
 roslaunch djiros djiros.launch & sleep 3; 
 roslaunch machine_defined ctrl_md.launch   
